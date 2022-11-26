64b5815c2e94202c2ede08098dcdc21b64934e0a
Glit <vpad.lazar.04@gmail.com>
2022-11-26 15:35:54 +0200

    Add project files.

---
+++ Lab_08_2/Lab_08_2.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+char* FindA(char* s, char* sp)
+{
+	char* word = NULL;
+	word = strtol
+	return t;
+}
+
+int main()
+{
+	const char* spc = " ";
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+	char* dest = new char[151];
+	dest = Change(str);
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+	return 0;
+}
