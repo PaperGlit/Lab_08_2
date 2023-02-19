99ea4fbdcefa887d5052a279218159bccaafe71c
Glit <vpad.lazar.04@gmail.com>
2023-02-19 20:50:31 +0200

    fix

--- Lab_08_2/Lab_08_2.cpp
+++ Lab_08_2/Lab_08_2.cpp
@@ -1,23 +1,28 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
-char* FindA(char* s, char* sp)
+char* FindA(char* s, const char* sp)
 {
 	char* word = NULL;
-	word = strtol
-	return t;
+	char* next_word = NULL;
+	word = strtok_s(s, sp, &next_word);
+	while (word != NULL)
+	{
+		if (word[0] == 'a' || word[0] == 'A')
+			return word;
+		word = strtok_s(NULL, sp, &next_word);
+	}
+	return (char*)"There are no words starting with 'a'";
 }
 
 int main()
 {
 	const char* spc = " ";
 	char str[101];
-	cout << "Enter string:" << endl;
+	cout << "Enter string: " << endl;
 	cin.getline(str, 100);
 	char* dest = new char[151];
-	dest = Change(str);
-	cout << "Modified string (param) : " << str << endl;
-	cout << "Modified string (result): " << dest << endl;
+	dest = FindA(str, spc);
+	cout << "Result: " << dest << endl;
 	return 0;
 }
---
+++ UnitTest_Lab_08_2/UnitTest_Lab_08_2.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_08_2/Lab_08_2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab082
+{
+	TEST_CLASS(UnitTestLab082)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const char* spc = " ";
+			char str[101] = "This is a test";
+			char* dest = FindA(str, spc);
+			Assert::AreEqual(strcmp("a", dest), 0);
+		}
+	};
+}
